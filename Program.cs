d2bee9f32c62912407591491dc7abc785a6f0827
Eklezyarez <eklezyarez@gmail.com>
2022-07-19 23:39:16 -0300

    case

--- Program.cs
+++ Program.cs
@@ -6,32 +6,37 @@ namespace C_Sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe a opção desejada:");
-            Console.WriteLine("1 - Inserir novo aluno");
-            Console.WriteLine("2 - Listar alunos");
-            Console.WriteLine("3 - Calcular média geral");
-            Console.WriteLine("0 - Sair");
-            Console.WriteLine();
-
-            string opcaoUsuario = Console.ReadLine();
+           
+            string opcaoUsuario = ObterOpcaoUsuario();
 
             while(opcaoUsuario != "0"){
                 switch (opcaoUsuario)
                 {
                     case "1":
-
+                        // Adcionar aluno
                     break;
 
                     case "2":
+                        // Listar alunos
                     break;
 
                     case "3":
+                        // calcular media geral
                     break;
 
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
 
+         
+
+            opcaoUsuario = ObterOpcaoUsuario();
+
+            }
+        }
+
+        private static string ObterOcaoUsuario(){
+
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar alunos");
@@ -40,8 +45,11 @@ namespace C_Sharp
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
 
-            }
         }
+
+
     }
+    
 }
